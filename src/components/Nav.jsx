b1245fa85702ad0df92efb9acdab0a7f79d54858
sumitrajpal29 <sumitrajpal29@users.noreply.github.com>
2025-06-07 16:51:00 +0530

    Fixed theme switcher and viewport

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react';
 import { FaGithub, FaLinkedin, FaEnvelope, FaMoon, FaSun } from 'react-icons/fa';
 
 export default function Nav() {
-    const [theme, setTheme] = useState(false);
-    document.body.className = theme ? "dark-theme" : "";
+    const [darkTheme, setDarkTheme] = useState(false);
+
+    const savedTheme = window.localStorage.theme;
+    document.body.className = savedTheme ? savedTheme : "";
 
     function toggleTheme() {
-        setTheme(!theme);
+        setDarkTheme(!darkTheme);
+        window.localStorage.setItem('theme', darkTheme ? "" : "dark-theme");
     }
     return (
         // <div>
@@ -25,7 +28,7 @@ export default function Nav() {
                     <a href="#">Contact</a>
                 </li>
             </ul>
-            <button className='roundButton' onClick={toggleTheme}>{theme ? <FaSun /> : <FaMoon />}</button>
+            <button className='roundButton' onClick={toggleTheme}>{darkTheme ? <FaSun /> : <FaMoon />}</button>
         </nav>
         // </div>
     );
